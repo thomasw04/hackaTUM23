92310ff08753f91d505ac978018eccde6bbb84d6
Philipp Erhardt <philipp.erhardt@tum.de>
2023-11-17 22:19:49 +0100

    Redirect all /craft requests to backend
    Co-authored-by: Raphael Klein <raffifasaro@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/craft*': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
