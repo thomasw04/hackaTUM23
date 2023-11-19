b95a80cbd4cee84fe025b4526db08c4312cf1bc9
Philipp Erhardt <philipp.erhardt@tum.de>
2023-11-19 08:20:39 +0100

    Fix postcode input page style

--- frontend/src/components/PostcodeInputPage.vue
+++ frontend/src/components/PostcodeInputPage.vue
@@ -6,8 +6,8 @@
 
 
     <div class="field pt-2">
-      <form id="search-form" action="/search" @submit.prevent>
-        <div id="search-container" class="custom-outline control has-icons-left has-icons-right is-large" :class="{ 'is-loading': isLoadingAutocomplete }">
+      <form id="search-form" class="custom-outline" action="/search" @submit.prevent>
+        <div id="search-container" class="control has-icons-left has-icons-right is-large" :class="{ 'is-loading': isLoadingAutocomplete }">
           <input autofocus id="search-bar" v-model="searchQuery" class="input is-large" type="text" placeholder="Postal code" autocomplete="off" @input="loadAutocomplete" @keydown.down.prevent="handleArrowDown" @keydown.up.prevent="handleArrowUp" @keydown.enter="handleEnter(false)" />
           <span class="icon is-small is-left">🔍</span>
           <span class="icon is-small is-right">{{}}</span>
@@ -15,7 +15,7 @@
 
         <div id="search-suggestions" v-if="showAutocomplete">
           <ul>
-            <li v-for="(result, index) in autocompleteResults" :key="result.zipcode" :class="{ 'is-active': index === activeAutocompleteIndex }">
+            <li v-for="(result, index) in autocompleteResults" :key="result.zipcode" class="result-list-item" :class="{ 'is-active': index === activeAutocompleteIndex }">
               <a class="navbar-item" @click="selectZipcode(index)" @mouseenter="setPreviewCoords(index)">
                 {{ result.zipcode }} {{ result.place }}
               </a>
@@ -23,7 +23,7 @@
           </ul>
         </div>
 
-        <div class="custom-outline pt-2">
+        <div class="pt-2">
           <button style="background-color: #0271c2" class="button is-primary is-fullwidth" @click="handleEnter(true)">Search</button>
         </div>
       </form>
@@ -129,7 +129,7 @@ export default defineComponent({
 }
 
 .is-active {
-  background-color: lightgray;
+  background-color: lightgray !important;
 }
 
 .custom-outline {
@@ -137,6 +137,11 @@ export default defineComponent({
   padding: 5px;
   border: 5px solid #f8b11e;
   border-radius: 8px;
+}
+
+.result-list-item {
+  background-color: #ffffff;
+  color: #5a5a5a;
 }
 
 #search-suggestions {
@@ -149,5 +154,11 @@ export default defineComponent({
     background-color: #4f4f4f !important;
     color: #aecdff !important;
   }
+
+  .result-list-item {
+    background-color: #212121;
+    color: #aecdff;
+  }
+
 }
 </style>
