e75fcfdee81984d1b778c3a01c81141ede146fb9
Philipp Erhardt <philipp.erhardt@tum.de>
2023-11-19 08:42:21 +0100

    Formatting

--- frontend/src/components/PostcodeInputPage.vue
+++ frontend/src/components/PostcodeInputPage.vue
@@ -4,18 +4,38 @@
       <SingleMap style="position: relative; min-height: 50vh" :coords="previewCoords" />
     </div>
 
-
     <div class="field pt-2">
       <form id="search-form" class="custom-outline" action="/search" @submit.prevent>
-        <div id="search-container" class="control has-icons-left has-icons-right is-large" :class="{ 'is-loading': isLoadingAutocomplete }">
-          <input autofocus id="search-bar" v-model="searchQuery" class="input is-large" type="text" placeholder="Postal code" autocomplete="off" @input="loadAutocomplete" @keydown.down.prevent="handleArrowDown" @keydown.up.prevent="handleArrowUp" @keydown.enter="handleEnter(false)" />
+        <div
+          id="search-container"
+          class="control has-icons-left has-icons-right is-large"
+          :class="{ 'is-loading': isLoadingAutocomplete }"
+        >
+          <input
+            autofocus
+            id="search-bar"
+            v-model="searchQuery"
+            class="input is-large"
+            type="text"
+            placeholder="Postal code"
+            autocomplete="off"
+            @input="loadAutocomplete"
+            @keydown.down.prevent="handleArrowDown"
+            @keydown.up.prevent="handleArrowUp"
+            @keydown.enter="handleEnter(false)"
+          />
           <span class="icon is-small is-left">🔍</span>
           <span class="icon is-small is-right">{{}}</span>
         </div>
 
         <div id="search-suggestions" v-if="showAutocomplete">
           <ul>
-            <li v-for="(result, index) in autocompleteResults" :key="result.zipcode" class="result-list-item" :class="{ 'is-active': index === activeAutocompleteIndex }">
+            <li
+              v-for="(result, index) in autocompleteResults"
+              :key="result.zipcode"
+              class="result-list-item"
+              :class="{ 'is-active': index === activeAutocompleteIndex }"
+            >
               <a class="navbar-item" @click="selectZipcode(index)" @mouseenter="setPreviewCoords(index)">
                 {{ result.zipcode }} {{ result.place }}
               </a>
@@ -24,7 +44,9 @@
         </div>
 
         <div class="pt-2">
-          <button style="background-color: #0271c2" class="button is-primary is-fullwidth" @click="handleEnter(true)">Search</button>
+          <button style="background-color: #0271c2" class="button is-primary is-fullwidth" @click="handleEnter(true)">
+            Search
+          </button>
         </div>
       </form>
     </div>
@@ -159,6 +181,5 @@ export default defineComponent({
     background-color: #212121;
     color: #aecdff;
   }
-
 }
 </style>
